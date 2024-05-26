85fa346f7dde291aadf2369a330c5a21e9e5909d
CODERELY07 <164634420+CODERELY07@users.noreply.github.com>
2024-05-26 16:00:13 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// side bar
+const sidebarToggle = document.querySelector('#sidebar-toggle');
+sidebarToggle.addEventListener('click', function(){
+    document.querySelector('#sidebar').classList.toggle("collapsed");
+})
